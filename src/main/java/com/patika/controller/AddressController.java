dd834f39fc9c015985401a0e22718dd2fa9f5b8b
Sinan Oral <sinnanoral@gmail.com>
2022-02-20 00:00:07 +0300

    Update urls and tags

--- src/main/java/com/patika/controller/AddressController.java
+++ src/main/java/com/patika/controller/AddressController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/addresses")
+@RequestMapping("/api/v1")
 public class AddressController {
 
     private final AddressService addressService;
@@ -31,32 +31,32 @@ public class AddressController {
         this.neighborhoodService = neighborhoodService;
     }
 
-    @Operation(summary = "gets all the addresses", tags = {"address"})
-    @GetMapping
+    @Operation(summary = "gets all the addresses", tags = {"addresses"})
+    @GetMapping("/addresses")
     public ResponseEntity<List<GetAddressResponse>> getAll() {
         return addressService.getAll();
     }
 
-    @Operation(summary = "gets an address by id", tags = {"address"})
-    @GetMapping("/{id}")
+    @Operation(summary = "gets an address by id", tags = {"addresses"})
+    @GetMapping("/addresses/{id}")
     public ResponseEntity<GetAddressResponse> getAddressById(@PathVariable Long id) {
         return addressService.getById(id);
     }
 
-    @Operation(summary = "creates an address", tags = {"address"})
-    @PostMapping
+    @Operation(summary = "creates an address", tags = {"addresses"})
+    @PostMapping("/addresses")
     public ResponseEntity<Void> createAddress(@RequestBody CreateAddressRequest address) {
         return addressService.create(address);
     }
 
-    @Operation(summary = "deletes an addresses by id", tags = {"address"})
-    @DeleteMapping("/{id}")
+    @Operation(summary = "deletes an address by id", tags = {"addresses"})
+    @DeleteMapping("/addresses/{id}")
     public ResponseEntity<Long> deleteAddressById(@PathVariable Long id) {
         return addressService.deleteById(id);
     }
 
     @Operation(summary = "creates a country", tags = {"countries"})
-    @PostMapping("/country")
+    @PostMapping("/countries")
     public ResponseEntity<Void> createCountry(@RequestBody CreateCountryRequest createCountryRequest) {
         return countryService.create(createCountryRequest);
     }
@@ -68,7 +68,7 @@ public class AddressController {
     }
 
     @Operation(summary = "creates a city", tags = {"cities"})
-    @PostMapping("/city")
+    @PostMapping("/cities")
     public ResponseEntity<Void> createCity(@RequestBody CreateCityRequest createCityRequest) {
         return cityService.create(createCityRequest);
     }
@@ -80,48 +80,48 @@ public class AddressController {
     }
 
     @Operation(summary = "gets districts of a city by city id", tags = {"cities"})
-    @GetMapping("/city/{id}/districts")
-    public ResponseEntity<List<GetDistrictResponse>> getDistrictsOfCityByCityId(@PathVariable Long id) {
-        return cityService.getDistrictsById(id);
+    @GetMapping("/cities/{id}/districts")
+    public ResponseEntity<List<GetDistrictResponse>> getDistrictsByCityId(@PathVariable Long id) {
+        return districtService.getDistrictsByCityId(id);
     }
 
     @Operation(summary = "creates a district", tags = {"districts"})
-    @PostMapping("/district")
+    @PostMapping("/districts")
     public ResponseEntity<Void> createDistrict(@RequestBody CreateDistrictRequest createDistrictRequest) {
         return districtService.create(createDistrictRequest);
     }
 
     @Operation(summary = "gets neighborhoods of a district by district id", tags = {"districts"})
-    @GetMapping("/district/{id}/neighborhoods")
-    public ResponseEntity<List<GetNeighborhoodResponse>> getNeighborhoodsOfDistrictByDistrictId(@PathVariable Long id) {
-        return districtService.getNeighborhoodsById(id);
+    @GetMapping("/districts/{id}/neighborhoods")
+    public ResponseEntity<List<GetNeighborhoodResponse>> getNeighborhoodsByDistrictId(@PathVariable Long id) {
+        return neighborhoodService.getNeighborhoodsByDistrictId(id);
     }
 
-    @Operation(summary = "creates a neighborhood", tags = {"neighborhood"})
-    @PostMapping("/neighborhood")
+    @Operation(summary = "creates a neighborhood", tags = {"neighborhoods"})
+    @PostMapping("/neighborhoods")
     public ResponseEntity<Void> createDistrict(@RequestBody CreateNeighborhoodRequest createNeighborhoodRequest) {
         return neighborhoodService.create(createNeighborhoodRequest);
     }
 
-    @Operation(summary = "updates name of a neighborhood by id", tags = {"neighborhood"})
-    @PutMapping("/neighborhood")
+    @Operation(summary = "updates name of a neighborhood by id", tags = {"neighborhoods"})
+    @PutMapping("/neighborhoods")
     public ResponseEntity<Void> updateNeighborhoodNameById(@RequestBody UpdateNeighborhoodRequest updateNeighborhoodRequest) {
         return neighborhoodService.updateNameById(updateNeighborhoodRequest);
     }
 
-    @Operation(summary = "gets streets of a neighborhood by neighborhood id", tags = {"neighborhood"})
-    @PutMapping("/neighborhood/{id}/streets")
-    public ResponseEntity<List<GetStreetResponse>> updateNeighborhoodNameById(@PathVariable Long id) {
-        return neighborhoodService.getStreetsById(id);
+    @Operation(summary = "gets streets of a neighborhood by neighborhood id", tags = {"neighborhoods"})
+    @GetMapping("/neighborhoods/{id}/streets")
+    public ResponseEntity<List<GetStreetResponse>> getStreetsByNeighborhoodId(@PathVariable Long id) {
+        return streetService.getStreetsByNeighborhoodId(id);
     }
 
-    @Operation(summary = "creates a street", tags = {"street"})
-    @PostMapping("/street")
+    @Operation(summary = "creates a street", tags = {"streets"})
+    @PostMapping("/streets")
     public ResponseEntity<Void> createStreet(@RequestBody CreateStreetRequest createStreetRequest) {
         return streetService.create(createStreetRequest);
     }
 
-    @Operation(summary = "updates name of a street by id", tags = {"street"})
+    @Operation(summary = "updates name of a street by id", tags = {"streets"})
     @PutMapping("/street")
     public ResponseEntity<Void> updateCityNameById(@RequestBody UpdateStreetRequest updateStreetRequest) {
         return streetService.updateNameById(updateStreetRequest);
