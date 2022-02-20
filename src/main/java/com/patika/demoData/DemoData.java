e59b3997efd4d348105b0b56197d1ee282d181b7
Sinan Oral <sinnanoral@gmail.com>
2022-02-20 23:04:16 +0300

    Rename mock to demoData

--- src/main/java/com/patika/demoData/DemoData.java
+++ src/main/java/com/patika/demoData/DemoData.java
@@ -1,4 +1,4 @@
-package com.patika.mock;
+package com.patika.demoData;
 
 import com.patika.dao.*;
 import com.patika.model.entity.*;
@@ -23,12 +23,12 @@ public class DemoData {
 
         System.out.println(event.getTimeTaken());
 
-        Country country = new Country(1L, "Turkiye", "TR");
+        Country country = new Country(1L, "Turkiye", "tr");
         City city = new City(1L, "Istanbul", "34", country);
         District district = new District(1L, "Maltepe", city);
         Neighborhood neighborhood = new Neighborhood(1L, "Findikli", district);
-        Street street = new Street(1L, "Sarmasik", neighborhood);
-        Address address = new Address(1L, country, city, district, neighborhood, street, "108", "3");
+        Street street = new Street(1L, "Cankaya", neighborhood);
+        Address address = new Address(1L, country, city, district, neighborhood, street, "4", "3");
 
         countryDao.save(country);
         cityDao.save(city);
